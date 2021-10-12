e5f7e9e72181ff2d57be8c9fd1fc55878da63442
mivlev1 <makzoyn@gmail.com>
2021-10-12 11:11:40 +0300

    edit js code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
-
 function total() {
-  let price,value;
+  let price;
+  let value;
   price=document.getElementById("price").value;
   value=document.getElementById("value").value;
-  //const regular = /^[1-9][0-9]*$^\d{1,}$/;
   const regular =/^[1-9][0-9]*$/;
- // const regular2 = /^[1-9][0-9]*$/;
   if (!regular.test(price) || !regular.test(value) ){
-  alert("Вводите только положительные числа");
+  alert("Вводите только положительные целые числа");
   return true;
   }
-  // else if(!regular2.test(price)&&!regular2.test(value))
-  // {
-  //   alert("Значения не введены");
-  //   return true;
-  // }
   let totalResult = document.getElementById("result");
   totalResult.innerHTML = parseInt(price) * parseInt(value);
   return false;
