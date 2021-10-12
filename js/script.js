5776b387875bd02f24b3fa227927e06ec991ed88
mivlev1 <makzoyn@gmail.com>
2021-10-12 11:00:28 +0300

    edit js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 
 function total() {
   let price,value;
-  price=document.getElementById('price').value;
-  value=document.getElementById('value').value;
-  const regular = /~^0+~^\d{1,}$/;
+  price=document.getElementById("price").value;
+  value=document.getElementById("value").value;
+  //const regular = /^[1-9][0-9]*$^\d{1,}$/;
+  const regular =/^[1-9][0-9]*$/;
+ // const regular2 = /^[1-9][0-9]*$/;
   if (!regular.test(price) || !regular.test(value) ){
-  alert("Вводите числа и не исользуйте в начале нули");
+  alert("Вводите только положительные числа");
+  return true;
   }
-  let totalResult = document.getElementById('result');
+  // else if(!regular2.test(price)&&!regular2.test(value))
+  // {
+  //   alert("Значения не введены");
+  //   return true;
+  // }
+  let totalResult = document.getElementById("result");
   totalResult.innerHTML = parseInt(price) * parseInt(value);
   return false;
 }
