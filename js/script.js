77b493cfb923fa12bbee2c28795c1be8a9c5dd5a
mivlev1 <makzoyn@gmail.com>
2021-10-13 13:37:04 +0300

    edit js file

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 function total() {
   let price;
   let value;
-  price=document.getElementById("price").value;
-  value=document.getElementById("value").value;
-  const regular =/^[1-9][0-9]*$/;
-  if (!regular.test(price) || !regular.test(value) ){
-  alert("Вводите только положительные целые числа");
-  return true;
+  price = document.getElementById("price").value;
+  value = document.getElementById("value").value;
+  const regular = /^[1-9][0-9]*$/;
+  if (!regular.test(price) || !regular.test(value)) {
+    alert("Вводите только положительные целые числа");
+    return true;
   }
   let totalResult = document.getElementById("result");
   totalResult.innerHTML = parseInt(price) * parseInt(value);
   return false;
 }
-window.addEventListener('DOMContentLoaded', function (event) {
+window.addEventListener('DOMContentLoaded', function (total) {
   console.log("DOM fully loaded and parsed");
   let b = document.getElementById("result-btn");
   b.addEventListener("click", total);
